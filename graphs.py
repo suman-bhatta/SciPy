77e6262f3f66bf4de2a2bff3ca90d812a5acd62f
Suman Bhatta <sumanbhatta735@gmail.com>
2025-01-04 21:35:46 +0545

    Add graphs module with examples for connected components and shortest path algorithms

---
+++ graphs.py
@@ -0,0 +1,102 @@
+"""
+Connected Components
+Example: Find all of the connected components with the connected_components() method.
+"""
+
+import numpy as np
+from scipy.sparse.csgraph import connected_components
+from scipy.sparse import csr_matrix
+
+arr = np.array([
+  [0, 1, 2],
+  [1, 0, 0],
+  [2, 0, 0]
+])
+
+newarr = csr_matrix(arr)
+
+print(connected_components(newarr))
+
+
+
+"""
+Dijkstra
+Example: Find the shortest path from element 1 to 2:
+"""
+
+import numpy as np
+from scipy.sparse.csgraph import dijkstra
+from scipy.sparse import csr_matrix
+
+arr = np.array([
+  [0, 1, 2],
+  [1, 0, 0],
+  [2, 0, 0]
+])
+
+newarr = csr_matrix(arr)
+
+print(dijkstra(newarr, return_predecessors=True, indices=0))
+
+
+
+"""
+Floyd Warshall
+Example: Find the shortest path between all pairs of elements:
+"""
+
+import numpy as np
+from scipy.sparse.csgraph import floyd_warshall
+from scipy.sparse import csr_matrix
+
+arr = np.array([
+  [0, 1, 2],
+  [1, 0, 0],
+  [2, 0, 0]
+])
+
+newarr = csr_matrix(arr)
+
+print(floyd_warshall(newarr, return_predecessors=True))
+
+
+"""
+Bellman Ford
+Example: Find shortest path from element 1 to 2 with given graph with a negative weight:
+"""
+
+import numpy as np
+from scipy.sparse.csgraph import bellman_ford
+from scipy.sparse import csr_matrix
+
+arr = np.array([
+  [0, -1, 2],
+  [1, 0, 0],
+  [2, 0, 0]
+])
+
+newarr = csr_matrix(arr)
+
+print(bellman_ford(newarr, return_predecessors=True, indices=0))
+
+
+
+"""
+Depth First Order
+Example: Traverse the graph depth first for given adjacency matrix:
+"""
+
+import numpy as np
+from scipy.sparse.csgraph import depth_first_order
+from scipy.sparse import csr_matrix
+
+arr = np.array([
+  [0, 1, 0, 1],
+  [1, 1, 1, 1],
+  [2, 1, 1, 0],
+  [0, 1, 0, 1]
+])
+
+newarr = csr_matrix(arr)
+
+print(depth_first_order(newarr, 1))
