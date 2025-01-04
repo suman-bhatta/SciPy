e7f2785a4ebc67d395e7674f67c58f8b168608c4
Suman Bhatta <sumanbhatta735@gmail.com>
2025-01-04 21:24:25 +0545

    Add sparse_data module with examples for CSR matrix operations

---
+++ sparse_data.py
@@ -0,0 +1,76 @@
+"""
+CSR Matrix
+We can create CSR matrix by passing an arrray into function scipy.sparse.csr_matrix().
+Exmple: Create a CSR matrix from an array:
+"""
+
+import numpy as np
+from scipy.sparse import csr_matrix
+
+arr = np.array([0, 0, 0, 0, 0, 1, 1, 0, 2])
+
+print(csr_matrix(arr))
+
+
+
+"""
+Sparse Matrix Methods
+Example: Viewing stored data (not the zero items) with the data property:
+"""
+
+import numpy as np
+from scipy.sparse import csr_matrix
+
+arr = np.array([[0, 0, 0], [0, 0, 1], [1, 0, 2]])
+
+print(csr_matrix(arr).data)
+
+
+# Example: Viewing the indices of the data with the indices property:
+
+import numpy as np
+from scipy.sparse import csr_matrix
+
+arr = np.array([[0, 0, 0], [0, 0, 1], [1, 0, 2]])
+
+print(csr_matrix(arr).count_nonzero())
+
+
+
+# Example: Eliminating zero items with the eliminate_zeros() method:
+
+import numpy as np
+from scipy.sparse import csr_matrix
+
+arr = np.array([[0, 0, 0], [0, 0, 1], [1, 0, 2]])
+
+mat = csr_matrix(arr)
+mat.eliminate_zeros()
+
+print(mat)
+
+
+# Example: Eliminating duplicates by adding them:
+
+import numpy as np
+from scipy.sparse import csr_matrix
+
+arr = np.array([[0, 0, 0], [0, 0, 1], [1, 0, 2]])
+
+mat = csr_matrix(arr)
+mat.sum_duplicates()
+
+print(mat)
+
+
+
+# Example: Eliminating duplicates by adding them:
+
+import numpy as np
+from scipy.sparse import csr_matrix
+
+arr = np.array([[0, 0, 0], [0, 0, 1], [1, 0, 2]])
+
+newarr = csr_matrix(arr).tocsc()
+
+print(newarr)
